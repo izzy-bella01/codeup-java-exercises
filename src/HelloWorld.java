973cd6e30520e9c0b8d4bbb94f728d6bfcf8331c
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-15 11:39:24 -0600

    completed first java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,24 @@
+// class - defining class
+// HelloWorld is the name of the class, and it matches the name of the .java file
 public class HelloWorld {
+
+    // method - similar to a function in javascript
+    // main method -> specific method in have that acts as our entry point when running java
+    // similar to starting point in a board game
+    // front door to get to any room in a house
+    public static void main (String[] args) {
+        // public, static, void -- pinned for later
+        // void -- for now, it means this method doesn't expect anything to be returned
+        // main is the name of our method and main is the keyword that is going to be searched as the entry point
+        // String[] args - passing in a string array named args
+
+        // print out "Hello World" to the console
+
+        System.out.print("Hello, World!");
+        System.out.print("\n");
+        // ^ same as
+            // System.out.println("Hello, World!");
+
+    }
+
 }
