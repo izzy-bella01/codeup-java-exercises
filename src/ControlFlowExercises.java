31d9fb8a9948bb152547353bcf3bc3f065faffe8
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-17 17:01:29 -0600

    last exercise for control flow

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -59,36 +59,112 @@ public class ControlFlowExercises {
 
 //  --------------TABLE OF POWERS-------------------
 
-        Scanner powers = new Scanner(System.in);
-
-        System.out.print("What number would you like to go up to? ");
-        int num = powers.nextInt();
-
-        System.out.println("Here is your table!");
-        System.out.println(" Number | Squared | Cubed");
-        for (int i = 1; i <= num; i++) {
-            int squared = (int) Math.pow(i, 2);
-            int cubed = (int) Math.pow(i, 3);
-
-            System.out.printf("| %s     | %s      | %s%n", i, squared, cubed);
-
-            if (num == i) {
-                Scanner continueTable = new Scanner(System.in);
-                System.out.print("Would you like to continue?  [y/N] ");
-                String userInput = continueTable.next();
-
-                if(userInput.equals("y")) {
-                    System.out.print("What number? ");
-                    num = powers.nextInt();
-                } else {
-                    System.out.println("Have a good day! Bye");
-                    break;
-                }
-            }
-        }
+//        Scanner powers = new Scanner(System.in);
+//
+//        System.out.print("What number would you like to go up to? ");
+//        int num = powers.nextInt();
+//
+//        System.out.println("Here is your table!");
+//        System.out.println(" Number | Squared | Cubed");
+//        for (int i = 1; i <= num; i++) {
+//            int squared = (int) Math.pow(i, 2);
+//            int cubed = (int) Math.pow(i, 3);
+//
+//            System.out.printf("| %s     | %s      | %s%n", i, squared, cubed);
+//
+//            if (num == i) {
+//                Scanner continueTable = new Scanner(System.in);
+//                System.out.print("Would you like to continue?  [y/N] ");
+//                String userInput = continueTable.next();
+//
+//                if(userInput.equals("y")) {
+//                    System.out.print("What number? ");
+//                    num = powers.nextInt();
+//                } else {
+//                    System.out.println("Have a good day! Bye");
+//                    break;
+//                }
+//            }
+//        }
 
 //  --------------GRADES-------------------
 
+//        Scanner start = new Scanner(System.in);
+//
+//        System.out.print("Want to see your grade? [y/n]: ");
+//        String showGrade = start.next();
+
+//        while(showGrade.equals("y")) {
+
+            Scanner grades = new Scanner(System.in);
+            do {
+                System.out.print("Enter a grade 0 - 100: ");
+                int userNum = grades.nextInt();
+
+                //            if (showGrade.equals("y")) {
+                if (userNum <= 100 && userNum >= 88) {
+                    System.out.println("A");
+                } else if (userNum <= 87 && userNum >= 80) {
+                    System.out.println("B");
+                } else if (userNum <= 79 && userNum >= 67) {
+                    System.out.println("C");
+                } else if (userNum <= 66 && userNum >= 60) {
+                    System.out.println("D");
+                } else if (userNum <= 59 && userNum >= 0) {
+                    System.out.println("F");
+                }
+            } while ();
+//                Scanner continueGrade = new Scanner(System.in);
+//                System.out.print("Would you like to continue?  [y/N] ");
+//                String userInput = continueGrade.next();
+//                while (userInput.equals("y")) {
+//                    System.out.print("What grade? ");
+//                    grades.nextInt();
+//                }
+            //}
+        //}
+
+
+
+//        Scanner grades = new Scanner(System.in);
+//        System.out.print("Enter a grade 0 - 100: ");
+//        int userNum = grades.nextInt();
+//
+//        while (showGrade.equals("y")) {
+//            if(userNum <= 100 && userNum >= 88) {
+//                System.out.println("A");
+//            } else if (userNum <= 87 && userNum >= 80) {
+//                System.out.println("B");
+//            } else if (userNum <= 79 && userNum >= 67) {
+//                System.out.println("C");
+//            } else if (userNum <= 66 && userNum >= 60) {
+//                System.out.println("D");
+//            } else if (userNum <= 59 && userNum >= 0) {
+//                System.out.println("F");
+//            }
+////            Scanner continueGrade = new Scanner(System.in);
+//            System.out.print("Would you like to continue?  [y/N] ");
+//            userNum = grades.nextInt();
+//
+//        }
+
+//        Scanner continueGrade = new Scanner(System.in);
+//        System.out.print("Would you like to continue?  [y/N] ");
+//        userNum = grades.nextInt();
+
+//        String userInput = continueGrade.next();
+
+//        if(userNum <= 100 && userNum >= 88) {
+//            System.out.println("A");
+//        } else if (userNum <= 87 && userNum >= 80) {
+//            System.out.println("B");
+//        } else if (userNum <= 79 && userNum >= 67) {
+//            System.out.println("C");
+//        } else if (userNum <= 66 && userNum >= 60) {
+//            System.out.println("D");
+//        } else if (userNum <= 59 && userNum >= 0) {
+//            System.out.println("F");
+//        }
 
     }
 }
