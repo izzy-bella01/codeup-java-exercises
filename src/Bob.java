244c868967b802d7ca63b974aa851ec9e7cb4437
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-18 11:07:50 -0600

    finished Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        String userInput = "";
+
+        do {
+            Scanner bobConvo = new Scanner(System.in);
+            System.out.print("Hi. I'm Bob. Talk to me. *Note: be sure to have proper punctuation* ");
+            String talk = bobConvo.nextLine();
+
+            if(talk.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (talk.endsWith("!")) {
+                System.out.println("Woah, chill out!");
+            } else if (talk.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            Scanner continueConvo = new Scanner(System.in);
+            System.out.print("Would you like to continue?  [y/N] ");
+            userInput = continueConvo.nextLine();
+        } while (userInput.equals("y"));
+    }
+}
