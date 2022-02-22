06449cb1cb825272d2f1b2feccf5f691eb042375
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-22 12:11:23 -0600

    created notes pages for oop

---
+++ src/notes/LectureEnvironment.java
@@ -0,0 +1,24 @@
+package notes;
+
+public class LectureEnvironment {
+    public static void main(String[] args) {
+        // using Person blueprint
+        Person thisPerson = new Person();
+
+        // now: instance variables to fulfill unique properties of a Person
+        thisPerson.firstName = "samuel";
+        thisPerson.lastName = "clemens";
+
+        System.out.println(thisPerson.sayHello());
+
+        // arithmetic: it wouldn't be proper to start of by making an instance of it
+//        Arithmetic kensMath = new Arithmetic();
+//        System.out.println(kensMath.pi);
+
+        // instead talk to it in a non-instance way
+        System.out.println(Arithmetic.pi);
+
+        System.out.println("Arithmetic.add(3,6) = " + Arithmetic.add(3,6));
+        System.out.println("Arithmetic.multiply(4,8) = " + Arithmetic.multiply(4, 8));
+    }
+}
