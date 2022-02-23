56cc499866d261e0e2fd4f06c3793cae48a2b0db
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-23 12:10:35 -0600

    finished input exercises for oop

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,6 +3,62 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getString() {
+        System.out.println("Type a sentence: ");
+        String sentence = scanner.nextLine();
+        return sentence;
+    }
+
+    public static boolean yesNo() {
+        System.out.println("Do you like doggos?");
+        String answer = scanner.nextLine();
+
+        if(answer.equals("y") || answer.equals("yes")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int getInt(int min, int max) {
+        System.out.print("Enter a number between " + min + " and " + max +": ");
+        int userInput = scanner.nextInt();
+
+        if(userInput >= min && userInput <= max) {
+            System.out.println("success");
+        } else {
+            System.out.println("no success");
+            getInt(min,max);
+        }
+        return userInput;
+    }
+
+    public static int getInt() {
+        System.out.print("Enter a number: ");
+        int userInput = scanner.nextInt();
+        return userInput;
+    }
+
+    public static double getDouble(double min, double max) {
+        System.out.print("Enter a decimal number between " + min + " and " + max +": ");
+        double userInput = scanner.nextDouble();
+
+        if(userInput >= min && userInput <= max) {
+            System.out.println("success");
+        } else {
+            System.out.println("no success");
+            getDouble(min,max);
+        }
+        return userInput;
+    }
+
+    public static double getDouble() {
+        System.out.print("Enter a decimal number: ");
+        double userInput = scanner.nextDouble();
+        return userInput;
+    }
+
 }
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,7 +1,14 @@
 package util;
 
+import static util.Input.*;
+
 public class InputTest {
     public static void main(String[] args) {
-
+        getString();
+        yesNo();
+        getInt(2,20);
+        getInt();
+        getDouble(0.5,58.6);
+        getDouble();
     }
 }
