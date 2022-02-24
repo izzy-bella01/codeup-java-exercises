76312788d33c6affb2c1ce1908360e180e338797
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-23 18:19:59 -0600

    finished oop exercises

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,10 +1,15 @@
 package shapes;
-import static util.Input.*;
+
+import java.util.Scanner;
 
 public class CircleApp {
     public static void main(String[] args) {
-        System.out.println();
-        double userRadius = scanner.nextDouble;
+        System.out.print("Enter radius: ");
+        Scanner enterRadius = new Scanner(System.in);
+        double userRadius = enterRadius.nextDouble();
 
+        Circle myCircle = new Circle(userRadius);
+        System.out.println("Area of your circle: " + myCircle.getArea());
+        System.out.println("Circumference of your circle: " + myCircle.getCircumference());
     }
 }
