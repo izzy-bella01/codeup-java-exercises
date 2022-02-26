043cb8c00f47fd77f5db56b3fea8587599733e31
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-25 23:17:09 -0600

    testing in ShapesTest

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,5 +3,13 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
+        Rectangle box1 = new Rectangle(5,4);
+        System.out.println("Area of box1: " + box1.getArea());
+        System.out.println("Perimeter of box1: " + box1.getPerimeter());
+
+        Rectangle box2 = new Square(5);
+        System.out.println("Area of box2: " + box2.getArea());
+        System.out.println("Perimeter of box2: " + box2.getPerimeter());
+
     }
 }
