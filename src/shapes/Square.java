ccc401052a85d28983807e7c462db8d6970274e2
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-26 22:13:08 -0600

    working on method overriding in ShapesTest

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,7 +2,24 @@ package shapes;
 
 public class Square extends Rectangle {
 
+//    protected int side;
+
     public Square(int side) {
         super(side, side);
     }
+
+    //perimeter = 4 x side
+    //area = side ^ 2
+
+//    public int getArea() {
+//        int area = side * side;
+//        System.out.println("area overriding: " + area);
+//        return area;
+//    }
+
+//    public int getPerimeter() {
+//        int perimeter = 4 * side;
+//        System.out.println("perimeter overriding: " + perimeter);
+//        return perimeter;
+//    }
 }
