1e3ef5e83a13925bf25d3d95d395c318df84ff94
Isabella <isabellarose.hernandez01@gmail.com>
2022-02-28 09:06:48 -0600

    completed shapes test exercise for inheritance and polymorphism

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,24 +2,25 @@ package shapes;
 
 public class Square extends Rectangle {
 
-//    protected int side;
+    protected int side;
 
     public Square(int side) {
         super(side, side);
+        this.side = side;
     }
 
     //perimeter = 4 x side
     //area = side ^ 2
 
-//    public int getArea() {
-//        int area = side * side;
-//        System.out.println("area overriding: " + area);
-//        return area;
-//    }
+    public int getArea() {
+        int area = side * side;
+        System.out.println("area overriding: " + area);
+        return area;
+    }
 
-//    public int getPerimeter() {
-//        int perimeter = 4 * side;
-//        System.out.println("perimeter overriding: " + perimeter);
-//        return perimeter;
-//    }
+    public int getPerimeter() {
+        int perimeter = 4 * side;
+        System.out.println("perimeter overriding: " + perimeter);
+        return perimeter;
+    }
 }
