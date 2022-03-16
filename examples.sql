ddfdbb62d5e1f3cdb8b54ff916c9842e5c8b3cef
Isabella <isabellarose.hernandez01@gmail.com>
2022-03-16 09:55:37 -0500

    updated commands list

--- examples.sql
+++ examples.sql
@@ -5,6 +5,25 @@ SELECT user, host FROM mysql.user
 SELECT * FROM mysql.help_topic
 SELECT help_topic_id, help_category_id, url FROM mysql.help_topic
 
+-- Finding the Current User
+mysql> SELECT current_user;
+
+-- Listing Users
+SELECT user, host FROM mysql.user;
+
+-- Creating a User
+CREATE USER 'billy'@'localhost' IDENTIFIED BY 'billysSecretP@ass123';
+
+-- Privileges
+GRANT ALL ON *.* TO 'billy'@'localhost'; -- grants all privileges on any and all database and table
+GRANT SELECT ON *.* TO 'read_only'@'localhost'; -- can grant specific privileges to any and all database and table
+GRANT SELECT, INSERT, UPDATE, DELETE ON sally_db.sallys_table TO 'sally'@'localhost'; -- grant to specific database and table
+GRANT ALL ON *.* TO 'billy'@'localhost' WITH GRANT OPTION; -- grant option to allow that user to grant to others
+SHOW GRANTS for 'username'@'hostname';
+
+-- Dropping a User
+DROP USER 'sally'@'localhost';
+
 -- Creating a Database
 CREATE DATABASE database_name;
 CREATE DATABASE IF NOT EXISTS database_name;
